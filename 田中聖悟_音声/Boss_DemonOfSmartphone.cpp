27071edf659f4e25d0123753d46861d51ffa3c06
TanakaShou <ohs30326@ohs.hal.ac.jp>
2025-01-11 13:20:19 +0900

    Add files via upload

---
+++ 田中聖悟_音声/Boss_DemonOfSmartphone.cpp
@@ -0,0 +1,26 @@
+#include"Boss_DemonOfSmartphone.h"
+/*
+void Enemy::Init(const wchar_t* imgname, int sx, int sy, float speed) {
+    Object::Init(imgname, sx, sy);
+    this->speed = speed;
+    this->isActive = true;
+}
+*/
+//UŒ‚
+void DemonOfSmartphone::EnemyAtack()
+{
+    if (IsActive() == true)
+    {
+        this->isActive = false;
+        int skill = 0;
+
+        //‹Z‘I‘ğ
+        
+    //L“Š‚°
+
+
+    //‰¹‹¿UŒ‚
+
+    }
+}
+
