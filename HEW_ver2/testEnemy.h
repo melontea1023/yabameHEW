57be654b0bf253733fc64be7ca7791d732de33da
Book-Bridge <ohs30337@ohs.hal.ac.jp>
2025-01-25 13:39:42 +0900

    行動パターン(仮)追加
    調整中です。

--- HEW_ver2/testEnemy.h
+++ HEW_ver2/testEnemy.h
@@ -1,6 +1,6 @@
 #pragma once
 #include"Object.h"
-
+#include"Advertisement raining.h"
 #include"t_bullet.h"
 
 
@@ -8,12 +8,14 @@ class TestEnemy :public Object
 {
 protected:
 
-	//Advertisement ad;
+	Advertisement ad;
 	Test_Bullet eb;
 
 	DirectX::XMFLOAT3 epos = { 0.0f,0.0f,0.0f };
 
 	DirectX::XMFLOAT3 ebpos = { 0.0f,0.0f,0.0f };
+
+	DirectX::XMFLOAT2 Set_E = { 0.0f,0.0f };
 
 	int aniCount = 0;
 	int finish_Count = 0;
@@ -21,12 +23,15 @@ protected:
 	DirectX::XMFLOAT2 link_e = { 0,0 };
 
 	int probability_Count = 0;
+
+	bool Ad_Animation_end_flg = false;; //広告降らしアニメーションが終わったかどうかの確認
+	bool Ed_Animation_end_flg = false;; //リンク投げアニメーションが終わったかどうかの確認
 public:
 
 	void CharacterInit(void);
 	void test_Update(DirectX::XMFLOAT3, DirectX::XMFLOAT3);
 	void CharacterDraw(void);
-	void Enemy_Action_Move(int);
+	void Enemy_Action_Move(int, DirectX::XMFLOAT3);
 	int probability(void);
 
 	//エネミーの広告降らしのアニメーション---------------------------------
@@ -34,5 +39,5 @@ public:
 	//---------------------------------------------------------------------
 	//エネミーのリンク投げのアニメーション---------------------------------
 	DirectX::XMFLOAT2 link_throwing_Animation();
-	//---------------------------------------------------------------------
+	//--------------------------------------------------------------------------
 };
--- HEW_ver2/test_move.cpp
+++ HEW_ver2/test_move.cpp
@@ -1,8 +1,7 @@
+//#include"Enemy.h"
 #include<stdlib.h>
 #include"Game.h"
 #include"time.h"
-
-//Enemy enemy;
 
 bool bullet_set_flg = true;
 bool bullet_move_flg = false;
@@ -12,17 +11,17 @@ void TestEnemy::CharacterInit(void)
 {
 
 	eb.Init(L"asset/link.png");   //リンクを初期化
-	eb.SetPos(SCREEN_WIDTH / 2 / 2, SCREEN_HEIGHT / 2 / 2, 0.0f);      //位置を設定
+	eb.SetPos(SCREEN_WIDTH / 2 / 2, SCREEN_HEIGHT / 2 / 2, 0.0f);//位置を設定
 	eb.SetSize(750.0f, 375.0f, 0.0f); //大きさを設定
 	eb.SetAngle(0.0f);   //角度を設定
 
+	ad.Advertisement_raining_Init();
 
 }
 
 
 void TestEnemy::test_Update(DirectX::XMFLOAT3 _pos, DirectX::XMFLOAT3 _nowepos)//左からPlayer,Enemyの座標
 {
-	/*DirectX::XMFLOAT3 bullet = eb.GetPos();*/
 
 	if (eb.loop_flg)
 	{
@@ -56,136 +55,159 @@ void TestEnemy::test_Update(DirectX::XMFLOAT3 _pos, DirectX::XMFLOAT3 _nowepos)/
 
 
 }
+
+void TestEnemy::Enemy_Action_Move(int _eMove_Type, DirectX::XMFLOAT3 _playerPosition)
+{
+	int e_Type = _eMove_Type;
+
+	switch (e_Type)
+	{
+	case 1://リンク投げに関係する処理を入れる
+
+		if (Ed_Animation_end_flg) //リンク投げアニメーションが終わっているかの確認
+		{
+			test_Update(_playerPosition, TestEnemy::GetPos());
+		}
+		else
+		{
+			link_throwing_Animation();
+			TestEnemy::SetUV(Set_E.x, Set_E.y);
+
+		}
+		break;
+	case 2://移動に関係する処理を入れる
+
+		break;
+	case 3://広告降らしに関係する処理を入れる
+
+
+
+		if (Ad_Animation_end_flg) //広告降らしアニメーションが終わっているかの確認
+		{
+			ad.Advertisement_raining_Update(_playerPosition);
+		}
+		else
+		{
+			Advertisement_raining_Animation();
+			TestEnemy::SetUV(Set_E.x, Set_E.y);
+		}
+
+		break;
+	}
+
+}
+
+
+void TestEnemy::CharacterDraw(void)
+{
+	/*enemy.Draw();*/
+	eb.Draw();
+	ad.Advertisement_raining_Darw();
+}
+
+
+//ボスの行動パターンメモ---------------------------
+//一回目：リンク投げ50％、広告降らし30％、移動20％
+//二回目：リンク投げ50％、広告降らし20％、移動30％
+//三回目：リンク投げ40％、広告降らし30％、移動30％
+//四回目：リンク投げ30％、広告降らし10％、移動60％
+//五回目：リンク投げ20％、広告降らし00％、移動80％
+//六回目：リンク投げ00％、広告降らし00％、移動100％
 //
-//void TestEnemy::Enemy_Action_Move(int _eMove_Type)
-//{
-//	int e_Type = _eMove_Type;
-//
-//	switch (e_Type)
-//	{
-//	case 1://リンク投げに関係する処理を入れる
-//
-//		break;
-//	case 2://移動に関係する処理を入れる
-//
-//		break;
-//	case 3://広告降らしに関係する処理を入れる
-//
-//		break;
-//	}
-//
-//}
-//
-//
-//void TestEnemy::CharacterDraw(void)
-//{
-//	/*enemy.Draw();*/
-//	eb.Draw();
-//}
-//
-//
-////ボスの行動パターンメモ---------------------------
-////一回目：リンク投げ50％、広告降らし30％、移動20％
-////二回目：リンク投げ50％、広告降らし20％、移動30％
-////三回目：リンク投げ40％、広告降らし30％、移動30％
-////四回目：リンク投げ30％、広告降らし10％、移動60％
-////五回目：リンク投げ20％、広告降らし00％、移動80％
-////六回目：リンク投げ00％、広告降らし00％、移動100％
-////
-////--------------------------------------------
-//
-//int TestEnemy::probability(void)
-//{
-//	int return_num = 0;
-//	srand(time(NULL));
-//	int r = (rand() % 10 + 1);
-//
-//	switch (probability_Count)
-//	{
-//	case 1:
-//		if (r <= 5)
-//		{
-//			return_num = 1;//リンク投げ
-//		}
-//		else if (r >= 6 && r <= 8)
-//		{
-//			return_num = 3;
-//		}
-//		else
-//		{
-//			return_num = 2;
-//		}
-//		probability_Count++;
-//		break;
-//	case 2:
-//		if (r <= 5)
-//		{
-//			return_num = 1;//リンク投げ
-//		}
-//		else if (r >= 6 && r <= 7)
-//		{
-//			return_num = 3;
-//		}
-//		else
-//		{
-//			return_num = 2;
-//		}
-//		probability_Count++;
-//		break;
-//	case 3:
-//		if (r <= 4)
-//		{
-//			return_num = 1;//リンク投げ
-//		}
-//		else if (r >= 5 && r <= 7)
-//		{
-//			return_num = 3;
-//		}
-//		else
-//		{
-//			return_num = 2;
-//		}
-//		probability_Count++;
-//		break;
-//	case 4:
-//		if (r <= 3)
-//		{
-//			return_num = 1;//リンク投げ
-//		}
-//		else if (r == 4)
-//		{
-//			return_num = 3;
-//		}
-//		else
-//		{
-//			return_num = 2;
-//		}
-//		probability_Count++;
-//		break;
-//	case 5:
-//		if (r <= 2)
-//		{
-//			return_num = 1; //リンク投げ
-//		}
-//
-//		else
-//		{
-//			return_num = 2;
-//		}
-//		probability_Count++;
-//		break;
-//	case 6:
-//		return_num = 2;
-//		probability_Count++;
-//		break;
-//	}
-//
-//
-//
-//
-//	if (probability_Count >= 7)
-//	{
-//		probability_Count = 1;
-//	}
-//	return return_num;
-//}
+//--------------------------------------------
+
+int TestEnemy::probability(void)
+{
+	int return_num = 0;
+	srand(time(NULL));
+	int r = (rand() % 10 + 1);
+
+	switch (probability_Count)
+	{
+	case 1:
+		if (r <= 5)
+		{
+			return_num = 1;//リンク投げ
+		}
+		else if (r >= 6 && r <= 8)
+		{
+			return_num = 3;
+		}
+		else
+		{
+			return_num = 2;
+		}
+		probability_Count++;
+		break;
+	case 2:
+		if (r <= 5)
+		{
+			return_num = 1;//リンク投げ
+		}
+		else if (r >= 6 && r <= 7)
+		{
+			return_num = 3;
+		}
+		else
+		{
+			return_num = 2;
+		}
+		probability_Count++;
+		break;
+	case 3:
+		if (r <= 4)
+		{
+			return_num = 1;//リンク投げ
+		}
+		else if (r >= 5 && r <= 7)
+		{
+			return_num = 3;
+		}
+		else
+		{
+			return_num = 2;
+		}
+		probability_Count++;
+		break;
+	case 4:
+		if (r <= 3)
+		{
+			return_num = 1;//リンク投げ
+		}
+		else if (r == 4)
+		{
+			return_num = 3;
+		}
+		else
+		{
+			return_num = 2;
+		}
+		probability_Count++;
+		break;
+	case 5:
+		if (r <= 2)
+		{
+			return_num = 1; //リンク投げ
+		}
+
+		else
+		{
+			return_num = 2;
+		}
+		probability_Count++;
+		break;
+	case 6:
+		return_num = 2;
+		probability_Count++;
+		break;
+	}
+
+
+
+
+	if (probability_Count >= 7)
+	{
+		probability_Count = 1;
+	}
+	return return_num;
+}
